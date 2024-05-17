0b2b68a695a7940ac33f0ce1454a17c189fa8446
jianhj <jianhongjun@umeweb.com>
2024-05-17 15:18:59 +0800

    http

--- run/gradio_ootd.py
+++ run/gradio_ootd.py
@@ -1,21 +1,20 @@
-import gradio as gr
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
+
+import gradio as gr
 import torch
-from PIL import Image, ImageOps
+from PIL import Image
 
 from utils_ootd import get_mask_location
 
 PROJECT_ROOT = Path(__file__).absolute().parents[1].absolute()
 sys.path.insert(0, str(PROJECT_ROOT))
 
-import time
 from preprocess.openpose.run_openpose import OpenPose
 from preprocess.humanparsing.run_parsing import Parsing
 from ootd.inference_ootd_hd import OOTDiffusionHD
 from ootd.inference_ootd_dc import OOTDiffusionDC
-
 
 openpose_model_hd = OpenPose(0)
 parsing_model_hd = Parsing(0)
@@ -25,10 +24,8 @@ openpose_model_dc = OpenPose(1)
 parsing_model_dc = Parsing(1)
 ootd_model_dc = OOTDiffusionDC(1)
 
-
 category_dict = ['upperbody', 'lowerbody', 'dress']
 category_dict_utils = ['upper_body', 'lower_body', 'dresses']
-
 
 example_path = os.path.join(os.path.dirname(__file__), 'examples')
 model_hd = os.path.join(example_path, 'model/model_1.png')
@@ -36,9 +33,10 @@ garment_hd = os.path.join(example_path, 'garment/03244_00.jpg')
 model_dc = os.path.join(example_path, 'model/model_8.png')
 garment_dc = os.path.join(example_path, 'garment/048554_1.jpg')
 
+
 def process_hd(vton_img, garm_img, n_samples, n_steps, image_scale, seed):
     model_type = 'hd'
-    category = 0 # 0:upperbody; 1:lowerbody; 2:dress
+    category = 0  # 0:upperbody; 1:lowerbody; 2:dress
 
     with torch.no_grad():
         garm_img = Image.open(garm_img).resize((768, 1024))
@@ -49,7 +47,7 @@ def process_hd(vton_img, garm_img, n_samples, n_steps, image_scale, seed):
         mask, mask_gray = get_mask_location(model_type, category_dict_utils[category], model_parse, keypoints)
         mask = mask.resize((768, 1024), Image.NEAREST)
         mask_gray = mask_gray.resize((768, 1024), Image.NEAREST)
-        
+
         masked_vton_img = Image.composite(mask_gray, vton_img, mask)
 
         images = ootd_model_hd(
@@ -67,6 +65,7 @@ def process_hd(vton_img, garm_img, n_samples, n_steps, image_scale, seed):
 
     return images
 
+
 def process_dc(vton_img, garm_img, category, n_samples, n_steps, image_scale, seed):
     model_type = 'dc'
     if category == 'Upper-body':
@@ -74,7 +73,7 @@ def process_dc(vton_img, garm_img, category, n_samples, n_steps, image_scale, se
     elif category == 'Lower-body':
         category = 1
     else:
-        category =2
+        category = 2
 
     with torch.no_grad():
         garm_img = Image.open(garm_img).resize((768, 1024))
@@ -85,7 +84,7 @@ def process_dc(vton_img, garm_img, category, n_samples, n_steps, image_scale, se
         mask, mask_gray = get_mask_location(model_type, category_dict_utils[category], model_parse, keypoints)
         mask = mask.resize((768, 1024), Image.NEAREST)
         mask_gray = mask_gray.resize((768, 1024), Image.NEAREST)
-        
+
         masked_vton_img = Image.composite(mask_gray, vton_img, mask)
 
         images = ootd_model_dc(
@@ -156,7 +155,7 @@ with block:
                     os.path.join(example_path, 'garment/04825_00.jpg'),
                 ])
         with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery", preview=True, scale=1)   
+            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery", preview=True, scale=1)
     with gr.Column():
         run_button = gr.Button(value="Run")
         n_samples = gr.Slider(label="Images", minimum=1, maximum=4, value=1, step=1)
@@ -164,10 +163,9 @@ with block:
         # scale = gr.Slider(label="Scale", minimum=1.0, maximum=12.0, value=5.0, step=0.1)
         image_scale = gr.Slider(label="Guidance scale", minimum=1.0, maximum=5.0, value=2.0, step=0.1)
         seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=-1)
-        
+
     ips = [vton_img, garm_img, n_samples, n_steps, image_scale, seed]
     run_button.click(fn=process_hd, inputs=ips, outputs=[result_gallery])
-
 
     with gr.Row():
         gr.Markdown("## Full-body")
@@ -204,7 +202,8 @@ with block:
                 ])
         with gr.Column():
             garm_img_dc = gr.Image(label="Garment", sources='upload', type="filepath", height=384, value=garment_dc)
-            category_dc = gr.Dropdown(label="Garment category (important option!!!)", choices=["Upper-body", "Lower-body", "Dress"], value="Upper-body")
+            category_dc = gr.Dropdown(label="Garment category (important option!!!)",
+                                      choices=["Upper-body", "Lower-body", "Dress"], value="Upper-body")
             example = gr.Examples(
                 label="Examples (upper-body)",
                 inputs=garm_img_dc,
@@ -245,7 +244,7 @@ with block:
                     os.path.join(example_path, 'garment/052234_1.jpg'),
                 ])
         with gr.Column():
-            result_gallery_dc = gr.Gallery(label='Output', show_label=False, elem_id="gallery", preview=True, scale=1)   
+            result_gallery_dc = gr.Gallery(label='Output', show_label=False, elem_id="gallery", preview=True, scale=1)
     with gr.Column():
         run_button_dc = gr.Button(value="Run")
         n_samples_dc = gr.Slider(label="Images", minimum=1, maximum=4, value=1, step=1)
@@ -253,8 +252,24 @@ with block:
         # scale_dc = gr.Slider(label="Scale", minimum=1.0, maximum=12.0, value=5.0, step=0.1)
         image_scale_dc = gr.Slider(label="Guidance scale", minimum=1.0, maximum=5.0, value=2.0, step=0.1)
         seed_dc = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=-1)
-        
+
     ips_dc = [vton_img_dc, garm_img_dc, category_dc, n_samples_dc, n_steps_dc, image_scale_dc, seed_dc]
     run_button_dc.click(fn=process_dc, inputs=ips_dc, outputs=[result_gallery_dc])
 
-block.launch(server_name='0.0.0.0', server_port=7865)
+# block.launch(server_name='0.0.0.0', server_port=7865)
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/test")
+def read_main():
+    return {"message": "This is your main app"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    app = gr.mount_gradio_app(app, block, path="/ootd")
+    uvicorn.run(app)
